c21ab057b33f36b6ea95e7a2a4936fc3974ed15e
nanaru <teamsinjito.nanaru@gmail.com>
2023-01-19 08:38:30 +0900

    feat: ユーザ詳細取得APIにバリデーション適用

--- html/src/Controller/Api/UserController.php
+++ html/src/Controller/Api/UserController.php
@@ -5,6 +5,8 @@ namespace App\Controller\Api;
 
 use App\Controller\AppController;
 use App\Exception\ApplicationException;
+use App\Response\Error\ValidationErrorResponse;
+use App\Request\ShowUserRequest;
 use OpenApi\Attributes as OA;
 
 class UserController extends AppController
@@ -16,10 +18,17 @@ class UserController extends AppController
      * @return \Illuminate\Http\Response
      */
     #[OA\Get(path: '/api/users/{id}', tags: ['User'])]
-    #[OA\PathParameter(required: true, name:'id', schema: new OA\Schema(type: 'integer'))]
+    #[OA\PathParameter(ref: '#/components/parameters/ShowUserRequest_id')]
     #[OA\Response(response: '200', description: 'The data', content: [new OA\JsonContent(required: ['name'], properties: [new OA\Property(property: 'name', type:'string', description:'ユーザ名')])])]
-    public function show($id): void
+    #[OA\Response(response: '400', description: 'The error', content: [new OA\JsonContent(ref: '#/components/responses/ValidationErrorResponse')])]
+    public function show(int $id): void
     {
+        $requestForm = new ShowUserRequest();
+        if (!$requestForm->execute(['id' => $id])) {
+            ValidationErrorResponse::error($this, $requestForm->getErrors());
+
+            return;
+        }
         // $input = new UserShowInput($id);
         // $res   = $this->user_service->show($input);
         $this->set('task', ['id' => 1]);
